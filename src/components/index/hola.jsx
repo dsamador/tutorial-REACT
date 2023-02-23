7118108322cfc169bb6c94ea8761561dbc0dda95
David Amador <54488581+dsamador@users.noreply.github.com>
2023-02-23 07:27:06 -0500

    informacion dinamica

--- src/components/index/hola.jsx
+++ src/components/index/hola.jsx
@@ -1,8 +1,27 @@
 
 
 export default function  Hola (){
+
+    let n1 = 5
+    let n2 = 7
+
+    const suma = ()=>{
+        console.log(n1+n2);
+    }
+
     return (
         <>
+            <button 
+                onClick={suma}
+                className="bg-blue-400
+                p-2 px-4 rounded-full 
+                focus:outline-none
+                focus:ring-2
+                focus:ring-offset-2
+                "
+            >
+                Sumar
+            </button>
             <div className="">Hola</div>
         </>
     )
